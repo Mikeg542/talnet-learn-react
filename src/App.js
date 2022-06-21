7d991b8d8973cc0b4feda2bab113c26d2896aaae
MichaelG <mgweb1@mail.com>
2022-06-21 08:29:15 -0400

    Clear old data, make into exercise

--- src/App.js
+++ src/App.js
@@ -2,37 +2,8 @@ import React from "react";
 
 import "./App.css";
 
-const App = () => {
-  const [counter, setCounter] = React.useState(0);
+import { appData } from "./data/appData";
 
-  const updateCounter = () => {
-    setCounter(counter + 1);
-  };
-
-  const doubledCounter = counter * 2;
-
-  return (
-    <div className="App">
-      <button onClick={updateCounter}>Update Counter</button> {counter}
-      <p>{`Twice Counter: ${doubledCounter}`}</p>
-      <input type="checkbox" checked />
-      {/*counter >= 5 ? (
-        <p>That's a lot of button pressing!</p>
-      ) : (
-        <p>Keep pressing that button!</p>
-      )}
-      {counter && (
-        <div
-          style={{
-            backgroundColor: "green",
-            width: 40,
-            height: 40,
-            textAlign: "center",
-          }}
-        />
-      )*/}
-    </div>
-  );
-};
+const App = () => {};
 
 export default App;
---
+++ src/data/appData.js
@@ -0,0 +1,82 @@
+export const appData = [
+	{
+		id: 0,
+		breed: "Abyssinian",
+		picture:
+			"https://cattime.com/assets/uploads/2011/12/file_2678_Abyssinian-cat-breed.jpg",
+		size: "M",
+		isFluffy: false,
+	},
+	{
+		id: 1,
+		breed: "Bengal",
+		picture:
+			"https://cattime.com/assets/uploads/2011/12/file_2706_bengal-cat-breed-picture.jpg",
+		size: "L",
+		isFluffy: false,
+	},
+	{
+		id: 2,
+		breed: "Devon Rex",
+		picture:
+			"https://cattime.com/assets/uploads/2011/12/file_2702_devon-rex-cat-breed-profile.jpg",
+		size: "M",
+		isFluffy: false,
+	},
+	{
+		id: 3,
+		breed: "Himalayan",
+		picture:
+			"https://cattime.com/assets/uploads/2011/12/file_2674_himalayan-460x290-460x290.jpg",
+		size: "M",
+		isFluffy: true,
+	},
+	{
+		id: 4,
+		breed: "Javanese",
+		picture:
+			"https://cattime.com/assets/uploads/2011/12/file_2730_javanese-460x290-460x290.jpg",
+		size: "S",
+		isFluffy: false,
+	},
+	{
+		id: 5,
+		breed: "Maine Coon",
+		picture:
+			"https://cattime.com/assets/uploads/2011/12/file_2680_Maine-Coon-Cat-breed.jpg",
+		size: "XL",
+		isFluffy: true,
+	},
+	{
+		id: 6,
+		breed: "Persian",
+		picture:
+			"https://cattime.com/assets/uploads/2011/12/file_2676_persian-460x290-460x290.jpg",
+		size: "M",
+		isFluffy: true,
+	},
+	{
+		id: 7,
+		breed: "Singapura",
+		picture:
+			"https://cattime.com/assets/uploads/2011/12/file_2746_singapura-460x290-460x290.jpg",
+		size: "S",
+		isFluffy: false,
+	},
+	{
+		id: 8,
+		breed: "Siamese",
+		picture:
+			"https://cattime.com/assets/uploads/2011/12/file_2667_siamese-cat-breed.jpg",
+		size: "S",
+		isFluffy: false,
+	},
+	{
+		id: 9,
+		breed: "Turkish Van",
+		picture:
+			"https://cattime.com/assets/uploads/2011/12/file_2742_turkish-van-cat-breed.jpg",
+		size: "L",
+		isFluffy: true,
+	},
+];
